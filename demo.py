ee39932dc3780d8a6f6fed8d7e0c64f71140da99
altope <altope58@gmail.com>
2015-08-26 11:34:26 -0300

    imagen de fondo
    poner una x imagen para verlo

--- demo.py
+++ demo.py
@@ -1,13 +1,13 @@
-import pygame,sys
-from pygame.locals import*
+import pygame
 def main():
     pygame.init()
     pantalla=pygame.display.set_mode([800,600])
+    imagen1 = pygame.image.load("pacman.png")
     salir=False
     reloj1=pygame.time.Clock()
     blanco=(240,255,252)
-    color=(250,88,55)
-    s1=pygame.Surface((800,100))
+    color=(150,254,55)
+    s1=pygame.Surface((20,10))
     s1.fill(color)
 
     while salir!=True:
@@ -17,9 +17,10 @@ def main():
             if evento.type == pygame.QUIT:
                 salir=True
 
-        reloj1.tick(20)
+        reloj1.tick(30)
         pantalla.fill(blanco)
-        pantalla.blit(s1,[10,500])
+        pantalla.blit(imagen1,(0,0))
+        pantalla.blit(s1,[100,200])
         pygame.display.update()
 
     pygame.quit()
